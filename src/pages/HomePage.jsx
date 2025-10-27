c2919d71c9897391baa25e233393d9f4cdd75e27
contact.johnjlawal <contact.johnjlawal@gmail.com>
2025-10-27 15:42:09 +0000

    edited

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import AboutSection from '../components/AboutSection';
-import ApplicationForm from '../components/ApplicationForm';
 
 const HomePage = () => {
   return (
